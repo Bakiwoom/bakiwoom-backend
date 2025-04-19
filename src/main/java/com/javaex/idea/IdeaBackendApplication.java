d1c729a6f74ee4bfac72ed6d781ba34a5fa9ec5e
H-JUYEONG <dud9902@gmail.com>
2025-04-19 13:28:31 +0900

    스프링 컴포넌트 스캔 범위를 com.javaex로 설정

--- src/main/java/com/javaex/idea/IdeaBackendApplication.java
+++ src/main/java/com/javaex/idea/IdeaBackendApplication.java
@@ -3,7 +3,7 @@ package com.javaex.idea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.javaex")
 public class IdeaBackendApplication {
 
     public static void main(String[] args) {
