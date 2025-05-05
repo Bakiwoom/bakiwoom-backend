ae5c69e426badab90bf667bde44668782eb1b2bd
soyoungJin44 <wlsthdud0412@naver.com>
2025-05-06 06:52:38 +0900

    token수정

--- src/main/java/com/javaex/config/WebMvcConfig.java
+++ src/main/java/com/javaex/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 	    registry.addMapping("/**")
-	        .allowedOriginPatterns("*","http://localhost:3000") // 모든 출처 허용 (특정 패턴 사용 가능)
+	        .allowedOriginPatterns("http://localhost:3000") // 모든 출처 허용 (특정 패턴 사용 가능) "*" 모든출처 지움
 	        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	        .allowedHeaders("*")
 	        .exposedHeaders("Authorization")
