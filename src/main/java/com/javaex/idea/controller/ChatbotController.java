63c2416c7b77b6827d54d99f7b8055f8e6bcca4d
antrophus <antrophus@yahoo.com>
2025-05-06 21:31:48 +0900

    ChatbotController 클래스에 @Autowired 어노테이션 추가

--- src/main/java/com/javaex/idea/controller/ChatbotController.java
+++ src/main/java/com/javaex/idea/controller/ChatbotController.java
@@ -6,6 +6,7 @@ import com.javaex.idea.dto.ConversationRequest;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
