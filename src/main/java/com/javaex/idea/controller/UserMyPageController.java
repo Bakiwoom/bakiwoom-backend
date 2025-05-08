fd68774123fae4192be89bc6fb53f7c7ffafb121
soyoungJin44 <wlsthdud0412@naver.com>
2025-05-08 22:32:21 +0900

    user mypage list수정

--- src/main/java/com/javaex/idea/controller/UserMyPageController.java
+++ src/main/java/com/javaex/idea/controller/UserMyPageController.java
@@ -1,12 +1,15 @@
 package com.javaex.idea.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.idea.service.UserMyPageService;
+import com.javaex.idea.vo.JobPostingVo;
 import com.javaex.idea.vo.UserVo;
 import com.javaex.util.JsonResult;
 
@@ -25,6 +28,15 @@ public class UserMyPageController {
 		}else {
 			return JsonResult.fail("실패");
 		}
+	};
+	
+	//user 지원현황 가져오기
+	@GetMapping(value="/api/mypage/getApplications/{userId}")
+	public JsonResult getApplications(@PathVariable ("userId") int userId) {
+		Map<String, Object> userApplicationMap = userMypageService.getApplications(userId);
+		
+		return JsonResult.success(userApplicationMap);
+		
 	};
 	
 	
--- src/main/java/com/javaex/idea/dao/UserMyPageDao.java
+++ src/main/java/com/javaex/idea/dao/UserMyPageDao.java
@@ -1,9 +1,12 @@
 package com.javaex.idea.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.javaex.idea.vo.JobPostingVo;
 import com.javaex.idea.vo.UserVo;
 
 @Repository
@@ -21,6 +24,18 @@ public class UserMyPageDao {
 	public UserVo getUserData(int userId) {
 		UserVo userData = sqlSession.selectOne("userMypage.getUserData",userId);
 		return userData;
+	};
+	
+	//지원공고 갯수 가져오기
+	public int getApplicationCount(int userId) {
+		int totalCount = sqlSession.selectOne("userMypage.getApplicationCount",userId);
+		return totalCount;
+	};
+	
+	//지원공고글list 가져오기
+	public List<JobPostingVo> getApplications(int userId) {
+		List<JobPostingVo> applicationList = sqlSession.selectList("userMypage.getApplications", userId);
+		return applicationList;
 	};
 	
 	
